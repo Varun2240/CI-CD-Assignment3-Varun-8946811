ac667b1c01163cef1fda155a17bdda97fd41e636
Varun Kumar Gundemoni <Vgundemoni6811@conestogac.on.ca>
2024-12-09 20:17:25 -0500

    Initial commit with Azure Function code

---
+++ _init_.py
@@ -0,0 +1,6 @@
+import azure.functions as func
+import logging
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    return func.HttpResponse("Hello, World!", status_code=200)
